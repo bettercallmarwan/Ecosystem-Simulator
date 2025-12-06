a32d6ae5521558292c3acaf8424f3f318dde0818
marwan <marwanossama2004@gmail.com>
2025-12-06 07:54:45 +0200

    nothing

--- AnimalSimulation.Functional/Program.cs
+++ AnimalSimulation.Functional/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 
 // ============================================
-// FUNCTIONAL PROGRAMMING APPROACH
+// FUNCTIONAL (declarative) PROGRAMMING APPROACH
 // ============================================
 
 
